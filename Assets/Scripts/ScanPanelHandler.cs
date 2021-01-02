3f5ba88bc97c453e0332849e906b867b68a121fa
KimYC1223 <kau_esc@naver.com>
2021-01-02 21:23:07 +0900

    Update minor fix

--- Assets/Scripts/ScanPanelHandler.cs
+++ Assets/Scripts/ScanPanelHandler.cs
@@ -83,7 +83,6 @@ public class ScanPanelHandler : MonoBehaviour
             colorBlock.selectedColor = BlueColor;
             ScanButtonText.text = "주변 BLE 기기 탐색 시작";
             RectTransform view = ScrollView.gameObject.GetComponent<RectTransform>();
-            view.sizeDelta = new Vector2(800f, 0f);
             BluetoothLEHardwareInterface.StopScan();
         }
         colorBlock.colorMultiplier = 1;
